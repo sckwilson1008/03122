f1d5d98dea590f0de9538bff6eb1168ab0d11e4f
sckwilson1008 <93990047+sckwilson1008@users.noreply.github.com>
2023-03-12 22:47:27 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,5 @@
-import os
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
+from flask import Flask
 app = Flask(__name__)
-
-
-
-
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-
-
-
-
-if __name__ == "__main__":
-    app.run()    
+    return "Hello, World!"
