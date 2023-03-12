c754d584e7d1dda373c9778d7bd13528b46b4166
sckwilson1008 <93990047+sckwilson1008@users.noreply.github.com>
2023-03-12 23:37:46 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,12 +5,28 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-CHANNEL_SECRET = "405c12ebd48d02117433cde09f727044"
-CHANNEL_ACCESS_TOKEN = "kwZ6RMTHwbtWUgP/pBUlDI8RJYgidnH1Zjj6EQllHDSz3yY4CUBboNpv9e4JjyojM047SNVapVg1utpll2vut6rdREiJvGGcHnGXrVNSPIji51UHILc2rUOXmlNCJEg7Sm8/2/8xzQOlBB0eWJFQ6gdB04t89/1O/w1cDnyilFU="
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 取得 Line 的 HTTP 請求
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        # 解析 Line 的 HTTP 請求，並處理訊息事件
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 回應使用者傳送的訊息
+    reply_message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, reply_message)
