ad1bcaf737d32f372d6bb6771854f58f05e4b5be
sckwilson1008 <93990047+sckwilson1008@users.noreply.github.com>
2023-03-12 23:44:43 +0800

    Update index.py

--- index.py
+++ index.py
@@ -11,44 +11,4 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-# 接收 LINE 的資訊
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    
-    try:
-        print(body, signature)
-        handler.handle(body, signature)
-        
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-# 學你說話
-@handler.add(MessageEvent, message=TextMessage)
-def pretty_echo(event):
-    
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        
-        # Phoebe 愛唱歌
-        pretty_note = '♫♪♬'
-        pretty_text = ''
-        
-        for i in event.message.text:
-        
-            pretty_text += i
-            pretty_text += random.choice(pretty_note)
-    
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=pretty_text)
-        )
-
-if __name__ == "__main__":
-    app.run()
-
 
