594b2c3eed36c24e6d92dc97d473314dbbeabc47
sckwilson1008 <93990047+sckwilson1008@users.noreply.github.com>
2023-03-12 22:45:49 +0800

    Update index.py

--- index.py
+++ index.py
@@ -14,28 +14,7 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
